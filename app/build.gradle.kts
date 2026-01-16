4f575420a518bfa04fbf2b3107def29e5ea15424
Duy Long Nguyen <longlunglay1998@gmail.com>
2026-01-17 08:44:57 +1100

    feat: Update versionCode to 3 and versionName to 1.1.1 for the upcoming release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.utility.cam"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
