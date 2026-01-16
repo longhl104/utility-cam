e886a32b81871d9698c56fa3e5131042f26e5eac
Duy Long Nguyen <longlunglay1998@gmail.com>
2026-01-17 09:52:44 +1100

    feat: Update versionCode to 6 and versionName to 1.1.4; enable edge-to-edge display in UtilityCamTheme

--- app/src/main/java/com/utility/cam/ui/theme/Theme.kt
+++ app/src/main/java/com/utility/cam/ui/theme/Theme.kt
@@ -2,6 +2,8 @@ package com.utility.cam.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -10,7 +12,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -44,9 +45,10 @@ fun UtilityCamTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
+            val activity = view.context as? ComponentActivity
+            activity?.enableEdgeToEdge()
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
