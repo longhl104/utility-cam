1639d5fbfc31eb76f430c0dc9d617abbb14e047f
Duy Long Nguyen <longlunglay1998@gmail.com>
2026-01-31 22:31:11 +1100

    feat: Update version to 1.7.1 and adjust AndroidManifest formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.utility.cam"
         minSdk = 26
         targetSdk = 36
-        versionCode = 54
-        versionName = "1.7.0"
+        versionCode = 55
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
